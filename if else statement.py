bf58d3ddb912c254a94d9d75335c5b24bf72c402
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2022-04-25 09:53:41 +0530

    Add files via upload

---
+++ if else statement.py
@@ -0,0 +1,17 @@
+#if else statement user defined
+
+a=int (input('Enter a number'))
+#a= int (a)
+
+c=a%2
+print(c)
+
+if(a==0):
+    print('no. is neither even nor odd')
+
+elif(a%2==0):
+    print('no. is even')    
+
+else:
+    print('no. is odd')
+    
