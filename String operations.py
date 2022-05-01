6b891b0b08490b2182c2ba9b400c77327f55d512
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2022-05-01 19:21:23 +0530

    Add files via upload

---
+++ String operations.py
@@ -0,0 +1,18 @@
+#slicing
+i="sakshi gayakwad"
+x=i[:5]
+print(x)
+
+#spliting
+y=i.split("i")
+print(y)
+
+#concatenation
+s1="inter"
+s2="national"
+print(s1+s2)
+
+#multiplication
+print(i*2)
+print(i*0)
+print(i*5)
