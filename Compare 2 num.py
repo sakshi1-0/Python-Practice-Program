16f85c30d584a9ed15fcfe20c1ecdc7b81e02d52
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2022-05-02 20:18:33 +0530

    Add files via upload

---
+++ Compare 2 num.py
@@ -0,0 +1,15 @@
+a=int(input('Enter first number'))
+b=int(input('Enter second number'))
+c=int(input('Enter third number'))
+
+if(a>b):
+    print('First number is greater than other two ')
+
+elif(b<c):
+    print('Third number is greater than other two')
+    
+else:
+    print('Second number is greater than other two')
+
+
+    
