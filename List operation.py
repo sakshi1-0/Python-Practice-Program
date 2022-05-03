0dabd2bda6f8f4294d0b1b5bd1342d472749a543
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2022-05-03 20:57:49 +0530

    Add files via upload

---
+++ List operation.py
@@ -0,0 +1,17 @@
+#list
+L1=[1,3,"ert"]
+print(L1)
+L1.insert(2,"ert")
+print(L1)
+L1.append(4)
+print(L1)
+w=L1.index(3)
+print("index of 3: ",w)
+
+#list with for loop
+print("list with loop")
+a=[ 1,3]
+x=len(a)
+for i in range(x):
+    print(a[i])
+    
