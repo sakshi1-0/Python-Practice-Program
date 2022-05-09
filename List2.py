a3251324b76161174379515141fd1f9042b2503d
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2022-05-09 11:25:35 +0530

    Add files via upload

---
+++ List2.py
@@ -0,0 +1,11 @@
+a=["sakshi",913573052,"sg48@gmail.com","indore"]
+print("Name-",a[0])
+print("Contact-",a[1])
+print("Email-",a[2])
+print("City-",a[3])
+print("\n")
+x=len(a)
+for i in range (x):
+    print(a[i])
+print("\n")
+y=a.index("Name")
