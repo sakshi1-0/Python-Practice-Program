6c96da6ef32025f1384a23a89950c727d5b60d5d
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2022-05-09 10:45:40 +0530

    Add files via upload

---
+++ class pratice.py
@@ -0,0 +1,9 @@
+class cars:
+    def __init__(self,b_name,model,types):
+        self.b_name=b_name
+        self.model=model
+        self.types=types
+   
+car1=cars("Maruti","Caiz","Sedan")
+c=car1.model
+print("Model of car 1: ",c)
