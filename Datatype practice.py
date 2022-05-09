5cd403fab4a1cb711d42d5e2582b68f1219afd95
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2022-05-09 11:31:47 +0530

    Add files via upload

---
+++ Datatype practice.py
@@ -0,0 +1,21 @@
+'''myset={1,5,8}
+print(myset)
+myset.add(4)
+print(myset)
+myset.update([9,8,7],{1,5,8,4})
+print(myset)
+s1="all is well be the change dont dive up"
+s2=s1.split()
+print(s2)
+
+s1="ert yui"
+s2=s1.split()
+print(s2)'''
+d={1:"one",2:"two"}
+print(d)
+d.copy()
+print(d)
+d2={1:"three",2:"four"}
+d.update(d2)
+print(d)
+
